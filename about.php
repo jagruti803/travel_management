6227bfda1bf16e4b5d6a91f0ada28f262b010df7
jagruti803 <jagrutiasawale@gmail.com>
2023-04-14 10:01:54 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+   <h1>about us</h1>
+</div>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="image">
+      <img src="images/about-img.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>why choose us?</h3>
+      <p>At Travel, we believe that Latest technology strengthens all successful, modern travel agencies.
+Traditional approach of people resources and quality does not work any longer.
+Travelopro is here to Digitally Transform your Business.
+Travel Agencies now increasingly expect technology to be the agent of change and expect us to devise strategies that are driven by ideas.
+Every one of our customer's portal is built on a strong, dynamic and innovative technology stack ensuring that our clients receive excellent service around the clock.</p>
+      <div class="icons-container">
+         <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>top destinations</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>affordable price</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-headset"></i>
+            <span>24/7 guide service</span>
+         </div>
+      </div>
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews">
+
+   <h1 class="heading-title"> clients reviews </h1>
+
+   <div class="swiper reviews-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>This website really works awesome.packages recomended are great.booking system is very easy i perfered ladhak ride</p>
+            <h3>pawan pawar</h3>
+            <span>traveler</span>
+            <img src="images/pawan.jpeg" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>This website really works awesome.packages recomended are great.booking system is very easy i perfered ladhak ride</p>
+            <h3>sahil shelar</h3>
+            <span>traveler</span>
+            <img src="images/sa.jpeg" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>This website really works awesome.packages recomended are great.booking system is very easy i perfered spiti valley</p>
+            <h3>jagruti asawale</h3>
+            <span>traveler</span>
+            <img src="images/ja.jpeg" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>This website really works awesome.packages recomended are great.booking system is very easy i perfered ladhak ride</p>
+            <h3>laxmiprasad patil</h3>
+            <span>traveler</span>
+            <img src="images/laxmi.jpeg" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>This website really works awesome.packages recomended are great.booking system is very easy i perfered ladhak ride</p>
+            <h3>samiksha suroshe</h3>
+            <span>traveler</span>
+            <img src="images/sam.jpeg" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>This website really works awesome.packages recomended are great.booking system is very easy i perfered ladhak ride</p>
+            <h3>lilesh asawale</h3>
+            <span>traveler</span>
+            <img src="images/lilesh.jpeg" alt="">
+         </div>
+
+      </div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> smiksha@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>mini project nhitm</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ package.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>package</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-2.png) no-repeat">
+   <h1>packages</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top destinations</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.webp" alt="">
+         </div>
+         <div class="content">
+            <h3>religious</h3>
+            <p>There are many different sorts of tourism, such as the religious tourism. It is that kind of tourism that deals with the religious places. Many cities of religious importance and sacred holy places marked the ancient background of the world. So, the globe is full of these blessed spots that are visited by millions of people. 
+</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>mountain</h3>
+            <p>The dynamic landscapes and thriving flora and fauna are major factors in defining the popularity of all the mountains of the world. For the more adventurous explorers, there are many other mountain ranges like the tallest mountain range, Mount Everest and the longest mountain in the world, Andes, which must be visited for an exhilarating experience of an adrenaline rush. </p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/ladhak.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventurous</h3>
+            <p>If you are looking for some awesome adventure destinations to add to your bucket list, you’ve come to the right place! It’s pretty hard to narrow down the most adrenaline-inducing places to travel.
+As an avid adventure traveler myself, Bu naturally, there are always TONS more places on anyone’s bucket list.
+</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/trek.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>trekking</h3>
+            <p>While travelling to different places is a refreshing experience, adding trekking to your vacation can make it more exciting. Trekking is a long walk to distant destinations. May be away from the beaten path and often on difficult terrain. Trekking gives the body a lot of exercise which improves blood flow and heart health. Good mental health accompanies physical health, enhancing your overall fitness.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-4.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>cities</h3>
+            <p>The world is a huge place full of wonderful people and places. The world is also a melting pot of different cultures and experiences so it can be hard to know where to start when you’re getting into traveling. Let us help you get your search started with the best cities in the world to visit.
+</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/beach.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>beaches</h3>
+            <p>India having the longest stretch of coastline exhibits some serene beautiful beaches. Important beaches in and around Mumbai include the Juhu Beach, Manori Beach, Alibaug, Kashid beach, Murud Janjira, Harihareshwar beach, Ganpatipule,
+Tarkarli, etc. Famous beaches trending towards the north of Mumbai are Mandvi beach, Daman, Diu, Dumas beach, Somnath, etc. Chennai being the beach town of India exhibits the true beauty of Marina beach.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/char dham yatra.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>char dham yatra</h3>
+            <p>Uttarakhand, also known as Devbhoomi or the Land of Gods, is home to numerous temples and welcomes devotees all year round. Among the countless religious sites and circuits that devotees visit in Uttarakhand, one of the most prominent is the Char Dham Yatra. This Yatra or pilgrimage is a tour of four holy sites - Yamunotri, Gangotri, Kedarnath and Badrinath – nestled high up in the Himalayas. In Hindi, 'char' means four and 'dham' refers to religious destinations.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/spiti valley.webp" alt="">
+         </div>
+         <div class="content">
+            <h3>spiti valley</h3>
+            <p>Situated in the higher reaches of the Himalayas, this park is home to endangered flora and fauna such as snow leopards, Siberian ibex, Himalayan snow cocks, Tibetan gazelle, snow partridges, and more. From admiring the beauty of nature to wildlife spotting, there is so much to do there that you would completely lose track of time.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/ladhak2.webp" alt="">
+         </div>
+         <div class="content">
+            <h3>ladakh</h3>
+            <p>The Land of High Passes, Ladakh, the newly created Union Territory in India (previously a part of Jammu & Kashmir), boasts a landscape that can leave you with your jaws dropped. It is a place where the landscape changes dramatically; it is truly astonishing to see the mountains, snow, cold desert, alpine meadows and lakes at a short distance from each other. Tourism in Ladakh is an absolute delight for nature lovers as well as adventurers.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/harihar fort.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>harihar fort</h3>
+            <p>There are two base villages of the fort, Harshewadi and Nirgudpada. The Harshewadi is 13 km from Trymbakeshwar. The other base village of the fort is Nirgudpada/Kotamvadi which is 40 km from Ghoti which itself is 48 km from Nashik and 121 km from Mumbai.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/pink jaipur city.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>pink jaipur city</h3>
+            <p>At the time of his rule, the Maharaja Sawai Ram Singh was one of the richest and most powerful men in the country. His true power was reflected by the law passed by him to maintain the pink colour for any future buildings in the city. The law was passed in 1877 because legend has it that the Queen of Jaipur was extremely fond of the terracotta pink colour.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/kokan.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>kokan</h3>
+            <p>Konkan is truly a heaven on earth for nature lovers who wish to relax in beaches, adventure to the top of hills and enjoy the beautiful waterfalls and sport in the clean backwaters. The 720 km long coastline along Maharashtra known as the Konkan is a stretch well known for it is beautiful beaches, greenery, hills and sea forts.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+
+<!-- packages section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> laxmi@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> ghansoli, india - 400104 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>mini project nhitm</span> | jagruti asawale </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
